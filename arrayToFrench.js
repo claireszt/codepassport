451a83da601b3c3e28d29d235391af8093715908
claireszt <claireszt@gmail.com>
2023-06-09 16:11:46 +0200

    first commit

---
+++ arrayToFrench.js
@@ -0,0 +1,18 @@
+const arrayToFrench = (array) => {
+    let string = ''
+   string += array[0]
+    
+    for (let i = 1 ; i < array.length ; i++) {
+
+        if (i < array.length-1) {
+            string += ', '+array[i]
+        } 
+        if (i === array.length-1) {
+            string += ' et ' + array[i]
+        }
+    }
+    
+    return 'Mon tableau contient '+string+'.'
+} 
+
+console.log(arrayToFrench(['tic','tac']))
