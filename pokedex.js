1158d59a1bb7bb77b15b8f11b05d1b3462bd72d2
claireszt <claireszt@gmail.com>
2023-06-13 16:13:39 +0200

    exercices objets

---
+++ pokedex.js
@@ -0,0 +1,13 @@
+const pokedex = [
+    {id: 1, name: 'Bulbizarre', evolution: 2, type: 'plante'},
+    {id: 2, name: 'Herbizarre', evolution: 3, type: 'plante'},
+    {id: 3, name: 'Florizarre', evolution: -1, type: 'plante'},
+    {id: 4, name: 'Salamèche', evolution: 5, type: 'feu'},
+    {id: 5, name: 'Reptincel', evolution: 6, type: 'feu'},
+    {id: 6, name: 'Dracaufeu', evolution: -1, type: 'feu'},
+    {id: 7, name: 'Carapuce', evolution: 8, type: 'eau'},
+    {id: 8, name: 'Carabaffe', evolution: 9, type: 'eau'},
+    {id: 9, name: 'Tortank', evolution: -1, type: 'eau'},
+];
+
+console.log(pokedex[0])
